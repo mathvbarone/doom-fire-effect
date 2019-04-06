ba966417dc4db027c9364121f341e980366abf19
Matheus Barone <matheusvbarone@gmail.com>
2019-04-06 13:05:46 -0300

    Editting server.js file

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/index.html'));
+app.use(express.static('./dist'));
 
 app.get('/*', function (req, res) {
 
-  res.sendFile(path.join(__dirname, '/dist/index.html'));
+  res.sendFile(path.join(__dirname, '/dist'));
 });
 
 // Start the app by listening on the default Heroku port
