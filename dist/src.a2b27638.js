36ad573100d05614892df214f73473f7ca623f34
Matheus Barone <matheusvbarone@gmail.com>
2019-04-07 14:16:54 -0300

    Finishing first version of the script

--- dist/src.a2b27638.js
+++ dist/src.a2b27638.js
@@ -119,26 +119,196 @@ parcelRequire = (function (modules, cache, entry, globalName) {
   return newRequire;
 })({"src/index.js":[function(require,module,exports) {
 var init = function init() {
-  var firePixelArray = [];
-  var fireWidth = 10;
-  var fireHeight = 10;
+  var firePixelsArray = [];
+  var fireWidth = 70;
+  var fireHeight = 70;
+  var fireColorsPalette = [{
+    "r": 7,
+    "g": 7,
+    "b": 7
+  }, {
+    "r": 31,
+    "g": 7,
+    "b": 7
+  }, {
+    "r": 47,
+    "g": 15,
+    "b": 7
+  }, {
+    "r": 71,
+    "g": 15,
+    "b": 7
+  }, {
+    "r": 87,
+    "g": 23,
+    "b": 7
+  }, {
+    "r": 103,
+    "g": 31,
+    "b": 7
+  }, {
+    "r": 119,
+    "g": 31,
+    "b": 7
+  }, {
+    "r": 143,
+    "g": 39,
+    "b": 7
+  }, {
+    "r": 159,
+    "g": 47,
+    "b": 7
+  }, {
+    "r": 175,
+    "g": 63,
+    "b": 7
+  }, {
+    "r": 191,
+    "g": 71,
+    "b": 7
+  }, {
+    "r": 199,
+    "g": 71,
+    "b": 7
+  }, {
+    "r": 223,
+    "g": 79,
+    "b": 7
+  }, {
+    "r": 223,
+    "g": 87,
+    "b": 7
+  }, {
+    "r": 223,
+    "g": 87,
+    "b": 7
+  }, {
+    "r": 215,
+    "g": 95,
+    "b": 7
+  }, {
+    "r": 215,
+    "g": 95,
+    "b": 7
+  }, {
+    "r": 215,
+    "g": 103,
+    "b": 15
+  }, {
+    "r": 207,
+    "g": 111,
+    "b": 15
+  }, {
+    "r": 207,
+    "g": 119,
+    "b": 15
+  }, {
+    "r": 207,
+    "g": 127,
+    "b": 15
+  }, {
+    "r": 207,
+    "g": 135,
+    "b": 23
+  }, {
+    "r": 199,
+    "g": 135,
+    "b": 23
+  }, {
+    "r": 199,
+    "g": 143,
+    "b": 23
+  }, {
+    "r": 199,
+    "g": 151,
+    "b": 31
+  }, {
+    "r": 191,
+    "g": 159,
+    "b": 31
+  }, {
+    "r": 191,
+    "g": 159,
+    "b": 31
+  }, {
+    "r": 191,
+    "g": 167,
+    "b": 39
+  }, {
+    "r": 191,
+    "g": 167,
+    "b": 39
+  }, {
+    "r": 191,
+    "g": 175,
+    "b": 47
+  }, {
+    "r": 183,
+    "g": 175,
+    "b": 47
+  }, {
+    "r": 183,
+    "g": 183,
+    "b": 47
+  }, {
+    "r": 183,
+    "g": 183,
+    "b": 55
+  }, {
+    "r": 207,
+    "g": 207,
+    "b": 111
+  }, {
+    "r": 223,
+    "g": 223,
+    "b": 159
+  }, {
+    "r": 239,
+    "g": 239,
+    "b": 199
+  }, {
+    "r": 255,
+    "g": 255,
+    "b": 255
+  }];
 
   var start = function start() {
     createFireDataStructure();
+    createFireSource();
     renderFire();
+    setInterval(calculateFirePropagation, 10);
   };
 
   var createFireDataStructure = function createFireDataStructure() {
     var numberOfPixels = fireWidth * fireHeight;
 
     for (var i = 0; i < numberOfPixels; i++) {
-      firePixelArray[i] = 0;
+      firePixelsArray[i] = 0;
     }
   };
 
-  var calculateFirePropagation = function calculateFirePropagation() {};
+  var calculateFirePropagation = function calculateFirePropagation() {
+    for (var column = 0; column < fireWidth; column++) {
+      for (var row = 0; row < fireHeight; row++) {
+        var pixelIndex = column + fireWidth * row;
+        updateFireIntensityPerPixel(pixelIndex);
+      }
+    }
+
+    renderFire();
+  };
+
+  var updateFireIntensityPerPixel = function updateFireIntensityPerPixel(currentPixelIndex) {
+    var belowPixelIndex = currentPixelIndex + fireWidth;
+    if (belowPixelIndex >= fireWidth * fireHeight) return;
+    var decay = Math.floor(Math.random() * 3);
+    var belowPixelFireIntensity = firePixelsArray[belowPixelIndex];
+    var newFireIntensity = belowPixelFireIntensity - decay >= 0 ? belowPixelFireIntensity - decay : 0;
+    firePixelsArray[currentPixelIndex - decay] = newFireIntensity;
+  };
 
   var renderFire = function renderFire() {
+    var debug = false;
     var html = '<table class="fireTable" cellpadding=0 callspacing=0>';
 
     for (var row = 0; row < fireHeight; row++) {
@@ -146,11 +316,19 @@ var init = function init() {
 
       for (var column = 0; column < fireWidth; column++) {
         var pixelIndex = column + fireWidth * row;
-        var fireIntensity = firePixelArray[pixelIndex];
-        html += '<td>';
-        html += "<div class=\"pixel-index\">".concat(pixelIndex, "</div>");
-        html += fireIntensity;
-        html += '</td>';
+        var fireIntensity = firePixelsArray[pixelIndex];
+
+        if (debug === true) {
+          html += '<td>';
+          html += "<div class=\"pixel-index\">".concat(pixelIndex, "</div>");
+          html += fireIntensity;
+          html += '</td>';
+        } else {
+          var color = fireColorsPalette[fireIntensity];
+          var colorString = "".concat(color.r, ",").concat(color.g, ",").concat(color.b);
+          html += "<td class=\"pixel\" style=\"background-color: rgb(".concat(colorString, ")\"");
+          html += '</td>';
+        }
       }
 
       html += '</tr>';
@@ -158,6 +336,14 @@ var init = function init() {
 
     html += '</table>';
     document.querySelector('#fireCanvas').innerHTML = html;
+  };
+
+  var createFireSource = function createFireSource() {
+    for (var column = 0; column <= fireWidth; column++) {
+      var overflowPixelIndex = fireWidth * fireHeight;
+      var pixelIndex = overflowPixelIndex - fireWidth + column;
+      firePixelsArray[pixelIndex] = 36;
+    }
   };
 
   start();
@@ -192,7 +378,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "56500" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64056" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
